c062e089bffad584511cd8a4c1e3b93d45c88f14
Magdalena Kasenberg <magdalena.kasenberg@codecoup.pl>
2025-06-27 07:36:01 +0200

    Revert "host: reset random address when starting"
    This reverts commit cd9f2d0937c709d19cc051c7c05488880bbdec97.
    The ble_hs_id_rnd_reset() call is now redundant.

--- nimble/host/src/ble_hs_startup.c
+++ nimble/host/src/ble_hs_startup.c
@@ -22,7 +22,6 @@
 #include "host/ble_hs.h"
 #include "host/ble_hs_hci.h"
 #include "ble_hs_priv.h"
-#include "ble_hs_id_priv.h"
 
 #if !MYNEWT_VAL(BLE_CONTROLLER)
 static int
@@ -408,8 +407,6 @@ ble_hs_startup_go(void)
     if (rc != 0) {
         return rc;
     }
-
-    ble_hs_id_rnd_reset();
 
     rc = ble_hs_startup_read_local_ver_tx();
     if (rc != 0) {
